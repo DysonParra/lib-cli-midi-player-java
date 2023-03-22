06af183713cd5fc67ad4147d205eb7938872727e
Dyson <dysontilano@gmail.com>
2023-03-22 12:55:35 -0500

    Updated license in source code files

--- src/main/java/com/project/dev/midi/player/MidiPlayer.java
+++ src/main/java/com/project/dev/midi/player/MidiPlayer.java
@@ -1,5 +1,5 @@
 /*
- * @fileoverview    {MidiPlayer} se encarga de realizar tareas específicas.
+ * @fileoverview    {MidiPlayer}
  *
  * @version         2.0
  *
--- src/main/java/com/project/dev/midi/player/MidiPlayerConstant.java
+++ src/main/java/com/project/dev/midi/player/MidiPlayerConstant.java
@@ -1,5 +1,5 @@
 /*
- * @fileoverview    {MidiPlayerConstant} se encarga de realizar tareas específicas.
+ * @fileoverview    {MidiPlayerConstant}
  *
  * @version         2.0
  *
